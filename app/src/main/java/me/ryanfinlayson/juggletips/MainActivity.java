72a6800e9d121d1654832463d0135da3487f62d5
RyanFin <21296921@student.uwl.ac.uk>
2016-12-04 13:31:58 +0000

    Theme Changes
    Changed the theme for a better look and feel

---
+++ app/src/main/java/me/ryanfinlayson/juggletips/MainActivity.java
@@ -0,0 +1,18 @@
+package me.ryanfinlayson.juggletips;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
