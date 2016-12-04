2b3853ea136385a6312cfdf4ad033edccd12c843
RyanFin <21296921@student.uwl.ac.uk>
2016-12-04 14:11:35 +0000

    Added String array for populating navigation drawer

--- app/src/main/java/me/ryanfinlayson/juggletips/MainActivity.java
+++ app/src/main/java/me/ryanfinlayson/juggletips/MainActivity.java
@@ -5,14 +5,21 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
+
+
+    String[] titles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
 
     }
 }
