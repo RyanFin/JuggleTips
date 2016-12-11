409d3d181ac29912d638bbfa7b1f8bb11dbad102
RyanFin <21296921@student.uwl.ac.uk>
2016-12-11 15:03:27 +0000

    Changes to the Overflow Menu Background Colour
    • Changed by using the android:itemBackground attribute

--- app/src/main/java/me/ryanfinlayson/juggletips/MainActivity.java
+++ app/src/main/java/me/ryanfinlayson/juggletips/MainActivity.java
@@ -1,19 +1,25 @@
 package me.ryanfinlayson.juggletips;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
 
     private String[] titles;
     private ListView drawerList;
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,23 @@ public class MainActivity extends Activity {
                 titles));
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        setIntent("Juggle Tips", "Enjoy our juggling app");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setIntent(String subject, String text){
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_SUBJECT, subject);
+        i.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(i);
+    }
+
 }
