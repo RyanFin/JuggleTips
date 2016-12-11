6d70fbe1fcd2ab7a67be6073a71e077079490141
RyanFin <21296921@student.uwl.ac.uk>
2016-12-11 14:29:59 +0000

    Populated Navigation Drawer
    • Populated the navigation drawer
    
    • Changed the drawer colour to grey

--- app/src/main/java/me/ryanfinlayson/juggletips/MainActivity.java
+++ app/src/main/java/me/ryanfinlayson/juggletips/MainActivity.java
@@ -7,17 +7,25 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-
-    String[] titles;
+    private String[] titles;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_activated_1,
+                titles));
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 
